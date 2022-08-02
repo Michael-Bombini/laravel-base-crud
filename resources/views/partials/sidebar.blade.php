afd581e3717497c8511c1688821e234c342fef02
Michael Bombini <bombini.michael@gmail.com>
2022-08-02 15:37:49 +0200

    Added sidebar partials

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,26 @@
+<div class="area"></div>
+<nav class="main-menu">
+    <ul>
+        <li>
+            <a href="">
+                <i class="fa fa-home fa-2x"></i>
+                <span class="nav-text">
+                    Home
+                </span>
+            </a>
+
+        </li>
+
+        <li>
+            <a href="">
+                <i class="fa fa-book fa-2x"></i>
+                <span class="nav-text">
+                    Comics
+                </span>
+            </a>
+
+        </li>
+       
+    </ul>
+
+</nav>
